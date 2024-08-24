3a73124dce05a115cd8874e8ba1e3b08eecb0fb5
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-24 18:14:48 +0300

    Modify header component styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import { Tooltip } from '@mui/material';
 import { IoMoonOutline } from '../constants/index';
+import styles from '../styles/padding.module.css';
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between border-b border-black border-opacity-10 bg-neutral-very-light-gray-bg py-6 drop-shadow-2xl xs:px-6 md:px-12 lg:px-24 dark:bg-neutral-dark-blue-elements dark:text-neutral-white-text-elements">
+    <header
+      className={`${styles.headerPadding} flex items-center justify-between border-b border-black border-opacity-10 bg-neutral-very-light-gray-bg py-6 drop-shadow-2xl dark:bg-neutral-dark-blue-elements dark:text-neutral-white-text-elements`}
+    >
       <p className="font-bold xs:text-xs sm:text-lg md:text-xl">Where in the World?</p>
 
       <Tooltip
