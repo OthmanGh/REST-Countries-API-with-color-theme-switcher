a95d87063064087eaea0f556e1572fde1bb086ab
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-24 18:15:07 +0300

    Add necessary packages & tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,15 @@ export default {
         semibold: 600,
         bold: 800,
       },
+
+      screens: {
+        xs: '280px', // Extra small devices (phones)
+        sm: '640px', // Small devices (tablets)
+        md: '768px', // Medium devices (small laptops)
+        lg: '1024px', // Large devices (desktops)
+        xl: '1280px', // Extra large devices (large desktops)
+        '2xl': '1536px', // 2x extra large devices (larger desktops and TVs)
+      },
     },
   },
   plugins: [],
