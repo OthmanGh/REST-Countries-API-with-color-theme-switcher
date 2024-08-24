d6cc397d4421fe8d1e4276b0bc768c33758d5374
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-24 18:11:07 +0300

    Create home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/padding.module.css';
+
+const Home = () => {
+  return <main className={` ${styles.headerPadding} h-screen dark:bg-neutral-very-dark-blue-bg`}>Home</main>;
+};
+
+export default Home;
