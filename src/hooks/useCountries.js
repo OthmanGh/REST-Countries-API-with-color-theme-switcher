39d2ac46314a91cf746c5b18a45eebc58d762672
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-24 22:42:40 +0300

    Create custom hook to fetch countries

---
+++ src/hooks/useCountries.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+const useCountries = () => {
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      setIsLoading(true);
+
+      try {
+        const res = await fetch('https://restcountries.com/v3.1/all');
+        if (!res.ok) throw new Error('Failed to fetch countries');
+        const data = await res.json();
+        setCountries(data);
+      } catch (error) {
+        setError(error?.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchCountries();
+  }, []);
+
+  return { countries, isLoading, error };
+};
+
+export default useCountries;
