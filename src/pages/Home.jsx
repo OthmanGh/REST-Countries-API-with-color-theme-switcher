2263b6c5648d5a65bdd9897b10a2b6fd364dc49f
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-24 22:43:12 +0300

    Seperate logic from home page interface

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
 import styles from '../styles/padding.module.css';
+import SearchField from '../ui/SearchField';
+import SelectByRegion from '../components/SelectByRegion';
+import Loader from '../components/Loader';
+import Error from '../components/Error';
+import useCountries from '../hooks/useCountries';
 
 const Home = () => {
-  return <main className={` ${styles.headerPadding} h-screen dark:bg-neutral-very-dark-blue-bg`}>Home</main>;
+  const { countries, isLoading, error } = useCountries();
+  if (error) return <Error message={error} />;
+
+  return (
+    <main className={` ${styles.headerPadding} relative flex h-screen flex-col gap-14 dark:bg-neutral-very-dark-blue-bg`}>
+      <div className="flex flex-col gap-6 sm:flex-row sm:items-center sm:justify-between">
+        <SearchField />
+        <SelectByRegion />
+      </div>
+
+      {isLoading ? <Loader /> : ''}
+    </main>
+  );
 };
 
 export default Home;
