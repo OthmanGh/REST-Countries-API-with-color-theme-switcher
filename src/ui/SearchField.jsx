7b1d1abea90ffc9f9cc7b37b896e64e09d118226
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-25 14:46:20 +0300

    Modify search field component styles

--- src/ui/SearchField.jsx
+++ src/ui/SearchField.jsx
@@ -1,11 +1,11 @@
 import SearchByCountry from '../components/SearchCountry';
 import { IoSearchSharp } from '../constants';
 
-const SearchField = () => {
+const SearchField = ({ onSearchQuery }) => {
   return (
-    <div className="relative flex max-w-[450px] items-center rounded-sm bg-neutral-very-dark-blue-text bg-opacity-5 p-1">
-      <IoSearchSharp className="top-50 absolute left-7 z-10 text-xl dark:text-gray-100" />
-      <SearchByCountry />
+    <div className="relative flex max-w-[450px] items-center rounded-md bg-neutral-very-light-gray-bg drop-shadow-md dark:bg-neutral-dark-blue-elements dark:text-neutral-white-text-elements dark:placeholder:text-gray-200">
+      <IoSearchSharp className="top-50 absolute left-7 z-10 text-xl text-gray-500 dark:text-gray-100" />
+      <SearchByCountry onSearchQuery={onSearchQuery} />
     </div>
   );
 };
