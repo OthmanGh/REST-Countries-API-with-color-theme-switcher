c079919554ede769a176c8b30dc70ab9030e0ae4
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-25 14:41:42 +0300

    Add styles for app component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Home from './pages/Home';
 
 const App = () => {
   return (
-    <>
+    <div className="min-h-screen bg-neutral-very-light-gray-bg dark:bg-neutral-very-dark-blue-bg">
       <Header />
       <Home />
-    </>
+    </div>
   );
 };
 
