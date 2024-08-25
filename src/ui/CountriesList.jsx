fcb5b2a1448583a205fda9bb3e3dcc1468b84f3d
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-25 14:43:45 +0300

    Integrate better user experience in case no country found

--- src/ui/CountriesList.jsx
+++ src/ui/CountriesList.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Country from './Country';
+import NoCountryFound from './NoCountryFound';
 
 const CountriesList = ({ countries }) => {
+  if (countries.length === 0) return <NoCountryFound />;
+
   return (
     <div className="grid grid-cols-1 gap-16 p-4 sm:grid-cols-2 sm:p-0 md:grid-cols-3 lg:grid-cols-4">
       {countries.map((country, i) => (
