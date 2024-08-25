dcf39c976e052f704b81c7e4b2de1d5cbad5ac99
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-25 11:51:26 +0300

    Create static no country found component

---
+++ src/ui/NoCountryFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoCountryFound = () => {
+  return (
+    <p className="p-2 text-center text-[16px] font-semibold text-gray-600 sm:mt-6 sm:text-start sm:text-xl dark:text-yellow-50">
+      Country Not Found - Try Different Input ⚠️
+    </p>
+  );
+};
+
+export default NoCountryFound;
