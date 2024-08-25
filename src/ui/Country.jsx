d4c4b90cef180ec72118df0b91304f2994c072ce
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-25 14:45:55 +0300

    Modify country component styles

--- src/ui/Country.jsx
+++ src/ui/Country.jsx
@@ -4,15 +4,13 @@ import React from 'react';
 const Country = ({ country }) => {
   const textColor = 'dark:text-neutral-300';
 
-  console.log(country);
-
   return (
-    <Tooltip placement="top" title="click to explore country details">
-      <div className="flex w-full cursor-pointer flex-col gap-4 overflow-hidden rounded-md drop-shadow-2xl transition-all duration-500  xs:hover:scale-95 sm:hover:scale-105 dark:bg-neutral-dark-blue-elements dark:text-neutral-white-text-elements">
+    <div className="flex w-full cursor-pointer flex-col gap-4 overflow-hidden rounded-md bg-neutral-very-light-gray-bg  drop-shadow-2xl transition-all duration-500 xs:hover:scale-95 sm:hover:scale-105 dark:bg-neutral-dark-blue-elements dark:text-neutral-white-text-elements">
+      <Tooltip placement="top" title="click to explore country details">
         <img src={country.flags.png} alt={`${country.name.official} flag`} className="o h-[160px] w-full object-cover" />
 
         <div className="px-6 py-3 text-sm">
-          <h2 className="mb-4 text-lg font-bold">{country.name.official}</h2>
+          <h2 className="mb-4 text-lg font-bold">{country.name.common}</h2>
 
           <div className="mb-3 flex flex-col gap-2">
             <p>
@@ -27,8 +25,8 @@ const Country = ({ country }) => {
             </p>
           </div>
         </div>
-      </div>
-    </Tooltip>
+      </Tooltip>
+    </div>
   );
 };
 
