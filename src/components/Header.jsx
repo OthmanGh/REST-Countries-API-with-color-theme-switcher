dc62738f9fcdb4c717efad93b5821c54d73298f5
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-25 14:31:39 +0300

    Modify header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,14 @@ import styles from '../styles/padding.module.css';
 const Header = () => {
   return (
     <header
-      className={`${styles.headerPadding} flex items-center justify-between border-b border-black border-opacity-10 bg-neutral-very-light-gray-bg py-6 drop-shadow-2xl dark:bg-neutral-dark-blue-elements dark:text-neutral-white-text-elements`}
+      className={`${styles.headerPadding} flex items-center justify-between border-b border-black border-opacity-5 bg-neutral-very-light-gray-bg  py-6 drop-shadow-2xl dark:bg-neutral-dark-blue-elements dark:text-neutral-white-text-elements`}
     >
-      <p className="font-bold xs:text-xs sm:text-lg md:text-xl">Where in the World?</p>
+      <p className="font-bold xs:text-[14px] sm:text-lg md:text-xl">Where in the World?</p>
 
       <Tooltip
         title="Toggle Dark/Light Mode"
         placement="bottom"
-        className="sm:text-md flex cursor-pointer items-center gap-2  font-semibold xs:text-xs md:text-lg"
+        className="sm:text-md flex cursor-pointer items-center gap-2  font-semibold xs:text-[13px] md:text-lg"
       >
         <IoMoonOutline />
         <span>Dark Mode</span>
