21d8043b41188f199d6feac82c90641a5b5c92a6
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-26 13:04:10 +0300

    Create & style back button

---
+++ src/components/BackButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { IoIosArrowRoundBack } from '../constants';
+import { useNavigate } from 'react-router-dom';
+
+const BackButton = () => {
+  const navigate = useNavigate();
+
+  return (
+    <button
+      className="duration-400 shadow-[hsl(200, 15%, 8%)] mb-20 flex h-10 w-28 items-center justify-center gap-1 rounded-sm bg-neutral-very-light-gray-bg font-light shadow-xl transition-all dark:bg-neutral-dark-blue-elements hover:dark:bg-neutral-dark-blue-elements-hover"
+      onClick={() => navigate(-1)}
+    >
+      <IoIosArrowRoundBack className="text-2xl" /> Back
+    </button>
+  );
+};
+
+export default BackButton;
