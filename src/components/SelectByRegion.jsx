bc2e93e711778228a1b36e783b912d980bd4d50d
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-27 16:31:12 +0300

    Implement handle selected region function

--- src/components/SelectByRegion.jsx
+++ src/components/SelectByRegion.jsx
@@ -1,14 +1,22 @@
 import { MdKeyboardArrowDown } from '../constants';
 
-const SelectByRegion = () => {
+const SelectByRegion = ({ onSelectedRegion }) => {
+  const handleSelectedRegion = (e) => {
+    onSelectedRegion(e.target.value);
+  };
+
   return (
     <div className="relative flex max-w-[200px] cursor-pointer items-center rounded-md bg-neutral-very-light-gray-bg px-6 text-gray-600 drop-shadow-md xs:h-12 sm:h-14 dark:bg-neutral-dark-blue-elements dark:text-neutral-white-text-elements ">
-      <select className="w-full cursor-pointer appearance-none rounded-lg bg-inherit pr-10 text-center outline-none sm:py-[14px]">
-        <option>Filter by region</option>
+      <select
+        className="w-full cursor-pointer appearance-none rounded-lg bg-inherit pr-10 text-center outline-none sm:py-[14px]"
+        onChange={handleSelectedRegion}
+      >
+        <option value="">Filter by region</option>
         <option value="africa">Africa</option>
         <option value="america">America</option>
         <option value="asia">Asia</option>
         <option value="europe">Europe</option>
+        <option value="oceania">Oceania</option>
       </select>
       <MdKeyboardArrowDown className="pointer-events-none absolute right-2 top-1/2 -translate-y-1/2 transform text-2xl" />
     </div>
