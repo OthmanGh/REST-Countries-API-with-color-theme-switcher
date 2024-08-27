bd812a984e9635a2ae9371dc8fa73f484b9472b3
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-27 16:31:47 +0300

    Add loading state

--- src/hooks/useCountryDetails.js
+++ src/hooks/useCountryDetails.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 const useCountryDetails = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const location = useLocation();
   const { capital, flags, languages, population, region, subregion, name, tld, currencies, borders } = location.state || {};
   const [borderCountries, setBorderCountries] = useState([]);
@@ -10,12 +11,15 @@ const useCountryDetails = () => {
   useEffect(() => {
     if (borders && borders.length > 0) {
       const fetchBorderCountries = async () => {
+        setIsLoading(true);
         try {
           const res = await Promise.all(borders.map((border) => axios.get(`https://restcountries.com/v3.1/alpha/${border}`)));
           const countryNames = res.map((response) => response.data[0].name.common);
           setBorderCountries(countryNames);
         } catch (error) {
           console.error('Error fetching border countries:', error);
+        } finally {
+          setIsLoading(false);
         }
       };
 
@@ -23,7 +27,7 @@ const useCountryDetails = () => {
     }
   }, [borders]);
 
-  return { borderCountries, capital, flags, languages, population, region, subregion, name, tld, currencies };
+  return { borderCountries, capital, flags, languages, population, region, subregion, name, tld, currencies, isLoading };
 };
 
 export default useCountryDetails;
