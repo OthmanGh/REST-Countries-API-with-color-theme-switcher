c8e9d876406fd675bbd0035fc0cb4946e8cc3d91
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-27 21:41:43 +0300

    Style app layout component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 const AppLayout = () => {
   return (
-    <div className="min-h-screen bg-neutral-very-light-gray-bg dark:bg-neutral-very-dark-blue-bg">
+    <div className="duration-400 min-h-screen bg-neutral-very-light-gray-bg transition-all dark:bg-neutral-very-dark-blue-bg">
       <Header />
       <Outlet />
     </div>
