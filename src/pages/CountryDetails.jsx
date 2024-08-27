b5de93170bf21e68693eebf8d05ed9d461c5e877
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-27 15:03:09 +0300

    Style country details page

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -7,67 +7,71 @@ const CountryDetail = () => {
   const { borderCountries, capital, flags, languages, population, region, subregion, name, tld, currencies } = useCountryDetails();
 
   return (
-    <div className="px-6 py-10 dark:text-white">
+    <div className="px-6 py-10 xl:px-20 dark:text-white">
       <BackButton />
 
-      <div className="flex flex-col gap-6 text-sm sm:grid sm:grid-cols-2">
-        <img src={flags.png} alt={`${name.common} flag`} className="mb-4 block h-64 w-full rounded-sm object-cover" />
+      <div className="flex flex-col gap-10 text-sm lg:grid lg:grid-cols-2 lg:gap-[5rem]">
+        <img src={flags.png} alt={`${name.common} flag`} className="h-72 w-full rounded-sm object-cover sm:h-96 sm:object-fill" />
 
-        <div className={`${flexLayout}`}>
-          <h2 className="mb-2 text-2xl font-bold">{name.common}</h2>
+        <div className="flex flex-col lg:p-10">
+          <h2 className="mb-6 text-2xl font-bold">{name.common}</h2>
 
-          <p>
-            Native Name: <span className={textColor}>Native Name</span>
-          </p>
-          <p>
-            Population: <span className={textColor}>{population}</span>
-          </p>
-          <p>
-            Region: <span className={textColor}>{region}</span>
-          </p>
-          <p>
-            Subregion: <span className={textColor}>{subregion}</span>
-          </p>
+          <div className="mb-10 gap-10 lg:flex lg:items-start lg:justify-between">
+            <div className={`${flexLayout} mb-10 lg:mb-0`}>
+              <p>
+                Native Name: <span className={textColor}>Native Name</span>
+              </p>
+              <p>
+                Population: <span className={textColor}>{population}</span>
+              </p>
+              <p>
+                Region: <span className={textColor}>{region}</span>
+              </p>
+              <p>
+                Subregion: <span className={textColor}>{subregion}</span>
+              </p>
 
-          <p>
-            Capital: <span className={textColor}>{capital}</span>
-          </p>
-        </div>
+              <p>
+                Capital: <span className={textColor}>{capital}</span>
+              </p>
+            </div>
 
-        <div className={flexLayout}>
-          <p>
-            Top Level Domain: <span className={textColor}>{tld}</span>
-          </p>
-          <p>
-            Currencies: {''}
-            <span className={textColor}>
-              {Object.values(currencies)
-                .map((currency) => currency.name)
-                .join(', ')}
-            </span>
-          </p>
-          <p>
-            Languages: {''}
-            <span className={textColor}>
-              {languages &&
-                Object.entries(languages).map(([code, language]) => (
-                  <span key={code}>
-                    {language}
-                    {', '}
-                  </span>
-                ))}
-            </span>
-          </p>
-        </div>
+            <div className={`${flexLayout} lg:mb-0`}>
+              <p>
+                Top Level Domain: <span className={textColor}>{tld}</span>
+              </p>
+              <p>
+                Currencies: {''}
+                <span className={textColor}>
+                  {Object.values(currencies)
+                    .map((currency) => currency.name)
+                    .join(', ')}
+                </span>
+              </p>
+              <p>
+                Languages: {''}
+                <span className={textColor}>
+                  {languages &&
+                    Object.entries(languages).map(([code, language]) => (
+                      <span key={code}>
+                        {language}
+                        {', '}
+                      </span>
+                    ))}
+                </span>
+              </p>
+            </div>
+          </div>
 
-        <div>
-          Border Countries:
-          <div className="mt-3 flex flex-wrap gap-4  dark:text-neutral-dark-gray-input">
-            {borderCountries.map((country) => (
-              <span className="shadow-[hsl(200, 15%, 8%)] rounded-sm px-5 py-[6px] shadow-md dark:bg-neutral-dark-blue-elements" key={country}>
-                {country}
-              </span>
-            ))}
+          <div>
+            Border Countries:
+            <div className="mt-3 flex flex-wrap gap-4  dark:text-neutral-dark-gray-input">
+              {borderCountries.map((country) => (
+                <span className="shadow-[hsl(200, 15%, 8%)] rounded-sm px-3 py-[4px] shadow-md dark:bg-neutral-dark-blue-elements" key={country}>
+                  {country}
+                </span>
+              ))}
+            </div>
           </div>
         </div>
       </div>
