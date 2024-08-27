bbd7cfa8a27afa11b4bdd2803db1b1e1c77ec5ad
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-27 17:39:49 +0300

    Pass country name to route path

--- src/ui/Country.jsx
+++ src/ui/Country.jsx
@@ -6,14 +6,14 @@ const Country = ({ country }) => {
   const navigate = useNavigate();
   const textColor = 'dark:text-neutral-300';
 
-  const handleClick = () => {
-    navigate('/country', { state: country });
+  const handleClick = (countryName) => {
+    navigate(`/country/${countryName}`, { state: country });
   };
 
   return (
     <div
       className="flex w-full cursor-pointer flex-col gap-4 overflow-hidden rounded-md bg-neutral-very-light-gray-bg  drop-shadow-2xl transition-all duration-500 xs:hover:scale-95 sm:hover:scale-105 dark:bg-neutral-dark-blue-elements dark:text-neutral-white-text-elements"
-      onClick={handleClick}
+      onClick={() => handleClick(country.name.common)}
     >
       <Tooltip placement="top" title="click to explore country details">
         <img src={country.flags.png} alt={`${country.name.official} flag`} className="o h-[160px] w-full object-cover" />
