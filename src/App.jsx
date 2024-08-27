b35ededb062705bd51460344621d0a937e17becd
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-27 21:04:43 +0300

    Wrapp app with theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import Home from './pages/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import AppLayout from './ui/AppLayout';
 import CountryDetail from './pages/CountryDetails';
+import ThemeProvider from './context/ThemeContext';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Home />} />
-          <Route path="country/:countryName" element={<CountryDetail />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path="country/:countryName" element={<CountryDetail />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
