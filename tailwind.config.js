b0f48214e4570b5a6292760a1e614a1e871495ec
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-27 15:06:33 +0300

    Modify tailwind config file & add necessary packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,10 @@ export default {
       colors: {
         neutral: {
           'dark-blue-elements': 'hsl(209, 23%, 22%)', // Dark Mode Elements
+          'dark-blue-elements-hover': 'hsl(209, 23%, 19%)', // Dark Mode Elements
           'very-dark-blue-bg': 'hsl(207, 26%, 17%)', // Dark Mode Background
           'very-dark-blue-text': 'hsl(200, 15%, 8%)', // Light Mode Text
-          'dark-gray-input': 'hsl(0, 0%, 52%)', // Light Mode Input
+          'dark-gray-input': 'hsl(0, 0%, 65%)', // Light Mode Input
           'very-light-gray-bg': 'hsl(0, 0%, 98%)', // Light Mode Background
           'white-text-elements': 'hsl(0, 0%, 100%)', // Dark Mode Text & L
         },
