e07c8df65fbc04ffdda8d4e9115eed38c15b72f0
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-27 21:03:16 +0300

    Style loader component based on theme status

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,14 @@
+import { useTheme } from '../context/ThemeContext';
 import React from 'react';
 import { RingLoader } from 'react-spinners';
 
 const Loader = () => {
+  const { theme } = useTheme();
+
   return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -transform">
-      <RingLoader color="rgb(229 231 235)" />
-      <p className="mt-6 text-sm dark:text-gray-200">Loading...</p>
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+      <RingLoader color={`${theme === 'dark' ? 'rgb(207 250 254)' : 'hsl(200 15% 8%)'}`} />
+      <p className="text-[hsl(200 15% 8%)] text-md mt-4 dark:text-cyan-100">Loading...</p>
     </div>
   );
 };
