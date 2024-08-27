bae1afdbc6b9affa62b4bcd705486593f4af95d0
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-27 15:05:46 +0300

    Add icons from react icons

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,5 +2,6 @@ import { IoMoon } from 'react-icons/io5';
 import { IoMoonOutline } from 'react-icons/io5';
 import { IoSearchSharp } from 'react-icons/io5';
 import { MdKeyboardArrowDown } from 'react-icons/md';
+import { IoIosArrowRoundBack } from 'react-icons/io';
 
-export { IoMoon, IoMoonOutline, IoSearchSharp, MdKeyboardArrowDown };
+export { IoMoon, IoMoonOutline, IoSearchSharp, MdKeyboardArrowDown, IoIosArrowRoundBack };
